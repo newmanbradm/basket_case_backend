bc2a9910bccf50c30d91beba9f27db789f14179a
newmanbradm <newmanbradm@gmail.com>
2018-08-02 14:24:59 -0400

    updated seeds, final backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
-brad = User.create(username: "newmanbradm")
-alex = User.create(username: "scruffles")
-risher = User.create(username: "coder3")
+foxx = User.create(username: "foxx")
 
-brad_score = Score.create(user_id: 1, point_total: 1229)
-alex_score = Score.create(user_id: 2, point_total: 1222)
-risher_score = Score.create(user_id: 3, point_total: 3400)
-brad_score2 = Score.create(user_id: 1, point_total: 2992)
+
+foxx_score = Score.create(user_id: 1, point_total: 100)
